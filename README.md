1f9c53a92c89f1cfa3c2cbb868eb8c3525825c27
Andrej Hosna <hosna.andrej@gmail.com>
2021-02-20 18:34:19 +0000

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # SaitekYokeMod
+This is relatively simple mod for improving Logitech Saitek yoke system. I figured it would be nice to have a bunch of switches on the yoke, because most of the time I'm playing VR and it's hard to reach with a controller behind the yoke. 
+Disassembling the yoke is just removing couple of screws and I've quickly figured out that there would be enough space for:
+- 9x On/Off flip switch - for lights, pitot, and something else
+- rotary encoder on the left for magneto/start switch
+- two red on/off for alt/batery master
+- two black on/off for avionics1/2 switch
+
+At first I've thought I'd use Arduino nano, but it was more practical to get cheap Leonardo clone with HID support. This allows the solution to look like just another game controller with plenty of buttons.
+
+# Parts
+- 10x small on of switch
+- 1x small rotary encoder with cap
+- 4x small red/black switches for master
+- arduino micro pro - AT32u4 based clone
+- prototype board
+- bunch of cables and connectors
+- 14x 1N4148 diodes 
+
+# HW 
+Buttons are arranged into 5x5 matrix with diodes to avoid ghosting. I have wired diodes directly to the switches, so there are not components on the proto board except for connectors. Google keypad matrix for more information. I'm using only 3 columns now since we only have 14 switches, but there's a potential for more in the future. Rotary encoder with its push button is wired directly as I had a component with +5v, gnd, btn, A, B pins and builtin pullups. I have space for another rotary encoder on the microcontroller. 10 pins are used for 5 rows and cols, 3+3 pins are used for two rotary encoders. It's very simple.
+
+# SW
+The software is a simple Arduino sketch. It uses following libraries:
+- Keypad
+- Joystick
+- RotaryEncoder 
+
+One note is that Keypad brings row down to sense buttons. Since I had the diodes soldered the other way around I had to swap rows/columns. Every switch is mapped to two joystick buttons. One for switch being flipped on and anoother for flipped off state. Swith 1 maps to button 1(off) and 2(on), switch 2 to 3(off) and 4(on). And so on.
+Rotary encoder is modified to click + and - buttons with totary encoder button being separate joystick button. Read the code, it's very simple.
+
+I was thinking of using Xplane interface directly but then I decided to go with more universal joystick support. This allows me to map this in both xplane and msfs.
+
+# Pictures
+!(Switches arrived)[01-switches.jpg]
+!(Placing switches into the case)[02-switches-placed.jpg]
+!(Proto board - 0 components)[03-proto-board.jpg]
+!(Look inside)[04-in-the-case.jpg]
+!(Rotary encoder for magnetos)[05-rotary-encoder.jpg]
+!(Making hole for master switches)[06-master-hole.jpg]
+!(Master and avionics)[07-master-btns.jpg]
+!(Finished mod)[08-finished-yoke-mod.jpg]
---
+++ SaitekYokeMod.ino
@@ -0,0 +1,126 @@
+#include <RotaryEncoder.h>
+
+#include <Keypad.h>
+#include <Joystick.h>
+#include <RotaryEncoder.h>
+
+const byte ROWS = 3;
+const byte COLS = 5;
+char keys[ROWS][COLS] = {
+  {0, 1, 2, 3, 4},
+  {5, 6, 7, 8, 64}, // last one not connected
+  {9, 10, 11, 12, 13},
+};
+const int BTN_DELAY = 100;
+// 26 joystick buttons from matrix
+const int JOY_IGN_DOWN = 26;
+const int JOY_IGN_UP = 27;
+const int JOY_IGN_BTN = 28;
+const int PIN_IGN_PUSH = 8;
+
+const byte ACTIVE_BUTTONS = 31;
+
+byte rowPins[ROWS] = {14, 15, 2}; // 3, 4}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {16, 18, 19, 20, 21}; //connect to the column pinouts of the keypad
+
+RotaryEncoder encoder(9, 10, RotaryEncoder::LatchMode::FOUR3);
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Joystick_ Joystick(
+   JOYSTICK_DEFAULT_REPORT_ID, 
+   JOYSTICK_TYPE_JOYSTICK, 
+   ACTIVE_BUTTONS, 
+   0, // # of HATs
+   false, // include X-axis
+   false, // include Y-axis
+   false, // include Z-axis
+   false, // include Rx-axis
+   false, // include Ry-axis
+   false, // include Rz-axis
+   false, // include Rudder
+   false, // include Throttle
+   false, // include Accelerator
+   false, // include Brake
+   false // include Steering
+);
+
+// First switch - btn 0 - to off , btn1 to on
+// Second switch - btn 2 - to off, btn3 to on
+// and so on
+int matrix_to_joy(char c, bool on) {
+  if (c>=64) return -1;
+  return (c*2) + int(on);  
+}
+
+void setup(){
+  Serial.begin(115200);
+  keypad.addEventListener(keypadEvent);
+  encoder.setPosition(0);
+  Joystick.begin();
+}
+
+void keypadEvent(KeypadEvent key){
+  int active_idx = keypad.findInList(key);
+  int state = keypad.key[active_idx].kstate;
+  if (state == PRESSED) {
+    Serial.println("ON");
+    Serial.println(keypad.key[active_idx].kchar, HEX);
+    int joy_id = matrix_to_joy(key, true);
+    Joystick.pressButton(joy_id);
+    delay(BTN_DELAY);
+    Joystick.releaseButton(joy_id);
+  }
+  if (state == RELEASED) {
+    Serial.println("OFF");
+    Serial.println(keypad.key[active_idx].kchar, HEX);
+    int joy_id = matrix_to_joy(key, false);
+    Joystick.pressButton(joy_id);
+    delay(BTN_DELAY);
+    Joystick.releaseButton(joy_id);
+  }
+}
+
+int change_ign(int diff) {
+  static int ign = 0;
+  if (diff < 0 && ign >0) {
+    ign += diff;
+    Serial.println("-");
+    Joystick.pressButton(JOY_IGN_DOWN);
+    delay(100);
+    Joystick.releaseButton(JOY_IGN_DOWN);
+  }
+  if (diff > 0 && ign <=4) {
+    ign += diff;
+    Serial.println("+");
+    Joystick.pressButton(JOY_IGN_UP);
+    delay(100);
+    Joystick.releaseButton(JOY_IGN_UP);
+  }
+}
+void check_ignition() {
+  static bool btn = true;
+  static int pos = 0;
+  encoder.tick();
+  int new_pos = encoder.getPosition();
+  if (new_pos != pos){
+    change_ign(new_pos-pos);
+  }
+  pos = new_pos;
+
+  bool btn_new = digitalRead(PIN_IGN_PUSH);
+  if (btn_new != btn) {
+    if (btn_new){
+      Serial.println("btn rlse");
+      Joystick.releaseButton(JOY_IGN_BTN);
+    }else{
+      Serial.println("btn push");
+      Joystick.pressButton(JOY_IGN_BTN);
+    }
+  }
+  btn = btn_new;
+}
+
+void loop(){
+  keypad.getKeys();
+  check_ignition();  
+  delay(10);
+}
